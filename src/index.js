b6ddc4ed040d484d477f4aebb7f4e84444009477
sergei2010 <sergejmatvejcuk323@gmail.com>
2020-11-23 22:50:02 +0200

    some ex. for hooks

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {Component, useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    const [value, setValue] = useState(0);
+    const [visible, setVisible] = useState(true);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    if (visible) {
+        return (
+          <div>
+              <button
+                onClick={() => setValue((v) => v + 1)}>
+                  +
+              </button>
+              <button
+                  onClick={() => setVisible(false)}>
+                hide
+              </button>
+              {/*<ClassCounter value={value} />*/}
+              {/*<HookCounter value={value} />*/}
+
+              <Notification />
+          </div>
+        );
+    } else {
+        return <button onClick={() => setVisible(true)}>
+            show
+        </button>
+    }
+};
+
+const HookCounter = ({ value }) => {
+
+
+    useEffect(() => {
+        console.log(' mount ');
+        return () => console.log(' unmount ');
+    }, []);
+    useEffect(() => console.log(' update '), );
+
+    return <p>{value}</p>;
+}
+
+class ClassCounter extends Component {
+    componentDidMount() {
+        console.log('class: mount');
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('class: update');
+    }
+    componentWillUnmount() {
+        console.log('class: unmount');
+    }
+    render() {
+        return <p>{this.props.value}</p>;
+    }
+}
+
+const Notification = () => {
+    const [visible, setVisible] = useState(true);
+    useEffect(() => {
+        const timeout = setTimeout(
+            () => setVisible(false), 2500);
+        return () => clearTimeout(timeout);
+    }, []);
+    return (
+        <div>
+            { visible && <p>Hello</p>}
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
